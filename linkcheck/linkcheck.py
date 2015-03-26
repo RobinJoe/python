c29259713245faecb327177c5da06a8d30b7d32f
kallimachos <kallimachos@gmail.com>
2015-03-26 15:00:48 +1000

    Updates to linkcheck.py

--- linkcheck/linkcheck.py
+++ linkcheck/linkcheck.py
@@ -25,6 +25,10 @@ ENDCOLOR = '\033[0m'
 
 #----------------------------------------------------------
 
+sumall = 0
+
+#----------------------------------------------------------
+
 def readData(infile):  # reads XML from file
     infile = path.realpath(infile)
     try:
@@ -34,7 +38,7 @@ def readData(infile):  # reads XML from file
         logging.error('File error (readData): ' + str(ioerr))
     return xml
 
-def checklinks(infile, count, total, verbose):
+def checklinks(infile, count, redirects, total, verbose):
     # check links and either display invalid links or display
     # count of valid and invalid links
 
@@ -45,9 +49,12 @@ def checklinks(infile, count, total, verbose):
     for ulink in soup('ulink'):
         allcount += 1
         url = str(ulink).split('"')[1]
-        if verbose: print("Checking link: " + url)
+        if verbose:
+            print(HEADER + path.basename(infile) + ENDCOLOR)
+            print("  Checking link: " + url)
         try:
-            status = requests.head(url).status_code
+            if redirects: status = requests.get(url).status_code
+            else: status = requests.head(url).status_code
             if status == requests.codes.ok:
                 status = OKGREEN + str(status)
                 goodcount += 1
@@ -55,18 +62,22 @@ def checklinks(infile, count, total, verbose):
                 status = FAIL + str(status)
                 badlinks.append(status + ENDCOLOR + ": " + url)
                 badcount += 1
-            if verbose: print("Status: " + status + ENDCOLOR)
+            if verbose: print("  Status: " + status + ENDCOLOR)
         except Exception as e:
-            print("Warning: " + str(e))
+            #print(WARNING + "Warning: " + ENDCOLOR + str(e))
+            pass
     if count:
-        if allcount > 0:
+        if badcount > 0:
             print(HEADER + path.basename(infile) + ENDCOLOR)
-            print("Total links: %s\n%sGood links: %s\n%sBad links: %s%s\n" % (allcount,OKGREEN,goodcount,FAIL,badcount,ENDCOLOR))
+            #print("Total links: %s\n%sGood links: %s\n%sBad links: %s%s\n" % (allcount,OKGREEN,goodcount,FAIL,badcount,ENDCOLOR))
+            print("%sBad links: %s%s\n" % (FAIL,badcount,ENDCOLOR))
     elif total:
         pass
-    elif badcount > 0:
+    elif badcount > 0 and not verbose:
         print(HEADER + path.basename(infile) + ENDCOLOR)
         for badlink in badlinks: print badlink
+    global sumall
+    sumall += allcount
     return badcount
 
 #----------------------------------------------------------
@@ -75,18 +86,21 @@ def checklinks(infile, count, total, verbose):
 def main():
     logConfig()
     parser = argparse.ArgumentParser(prog="linkcheck", description="Check ulinks in DocBook XML.")
-    parser.add_argument('INPUT', type=str, help='input XML file or directory containing XML files')
+    parser.add_argument('INPUT', type=str, help='XML file or directory containing XML files')
     parser.add_argument('-c', '--count', action='store_true', default=False, help='Print count of invalid links per file')
-    parser.add_argument('-t', '--total', action='store_true', default=False, help='Print the total number of invalid links')
+    parser.add_argument('-r', '--redirects', action='store_true', default=False, help='Follow 301 redirects and treat redirect target as link. SLOW.')
+    parser.add_argument('-t', '--total', action='store_true', default=False, help='Print only the total number of invalid links')
     parser.add_argument('-v', '--verbose', action='store_true', default=False, help='Verbose output')
     args = parser.parse_args()
     badtotal = 0
+    print("Checking links...\n")
     if path.isfile(args.INPUT):
-        badtotal = checklinks(args.INPUT, args.count, args.total, args.verbose)
+        badtotal = checklinks(args.INPUT, args.count, args.redirects, args.total, args.verbose)
     else:
         for filename in iglob(args.INPUT + '*.xml'):
-            badtotal += checklinks(filename, args.count, args.total, args.verbose)
-    print("Total of all broken links: " + str(badtotal))
+            badtotal += checklinks(filename, args.count, args.redirects, args.total, args.verbose)
+    print("\nLinks checked: " + str(sumall))
+    print("Broken links: " + str(badtotal) + "\n")
 
 #===========================================================
 # Logging Configuration
