2e36fa1cdde5c9ea11fd89a50d990524cc5d7c5c
kallimachos <kallimachos@gmail.com>
2016-01-11 11:46:13 +1000

    Finish table.py and add comments

--- table/table.py
+++ table/table.py
@@ -1,10 +1,54 @@
 #!/bin/python3
+"""
+Converts RST grid tables into list-tables.
 
-# Script for converting RST grid tables into list-tables
+Basic usage:
 
-import logging
-import re
+    1. Copy the grid table into a file, e.g. 'input.rst'
+    2. Convert the grid table to a list-table. The result is output to stdout:
+
+       $ table input.rst
+
+Options:
+
+    -o [FILE]   Write the converted table to [FILE]
+    -t [TITLE]  Use [TITLE] as the table title
+
+Warning:
+
+    This script does not handle multiline cells. If you convert a table with
+    multiline cells you may receive an error like this when running
+    sphinx-build:
+
+    ERROR: Error parsing content block for the "list-table" directive: uniform
+           two-level bullet list expected, but row 2 does not contain the same
+           number of items as row 1 (4 vs 3)
+
+    This indicates that the list-table needs manual clean-up. Look for a lines
+    like this in the source:
+
+       * - Category
+         - Privilege
+         - Available
+       * - Alarms
+         - Acknowledge an alarm    * - hello
+         -
+         - Yes
+
+    Compare with the original table to determine the correct structure of the
+    broken row.
+
+Notes:
+
+    The script sets all columns to the same width: 100 / col_num. After
+    conversion, you may want to manually edit :width:
+
+    The script automatically uses the first row of the table as a header.
+    After covervsion, you may want to manually edit :header-rows:
+"""
+
 import argparse
+import logging
 from os import path
 
 home = path.expanduser("~")
@@ -13,64 +57,56 @@ script_dir = path.dirname(path.realpath(__file__))
 # ----------------------------------------------------------
 
 
-def readRST(infile):
-    """Read RST data from file and return array."""
+def readfile(infile):
+    """Read data from file and return string."""
     infile = path.realpath(infile)
     try:
-        with open(infile, 'rb') as f:
+        with open(infile, 'r') as f:
             data = f.read()
     except IOError as ioerr:
         logging.error('File error (readData): ' + str(ioerr))
-    data = data.decode("utf-8")
     return(data)
 
 
-def writeRST(outfile, data):
-    """Write RST list-table to stdout or (optional) file."""
+def writefile(outfile, data):
+    """Write data to file."""
     outfile = path.realpath(outfile)
     try:
-        with open(outfile, 'wb') as f:
+        with open(outfile, 'w') as f:
             f.write(data)
     except IOError as ioerr:
         logging.error('File error (writeData): ' + str(ioerr))
 
 
 def adjustRow(row, col_num):
+    """Convert a grid row to a list-table row."""
     if row.startswith('+') is True:
-        firstline = '  * '
-    else:
-        firstline = ''
-    col = row.split('|')
-    new_col = []
-    for entry in col:
-        new_col.append(entry) # stip this, but while leaving blank strings where required.
+        return('\n')
+    row = row.split('|')
+    new_row = []
+    for entry in row:
+        new_row.append(entry)
         try:
-            new_col.pop(new_col.index(''))
+            new_row.pop(new_row.index(''))
         except:
             pass
-
-    print(new_col)
-    # new_row = firstline + '\n'
-    # return(new_row)
+    convert = []
+    convert.append('  * - ' + new_row[0].strip())
+    for entry in new_row[1:]:
+        convert.append('\n     - ' + entry.strip())
+    result = ' '.join(convert)
+    return(result)
 
 
 def buildTable(infile, outfile, title):
-    """
-    Build the RST list-table.
-
-    * - Category
-      - Privilege
-      - Available?
-    * - Alarms
-      - Acknowledge an alarm
-      - Yes
-    """
-    data = readRST(infile)
+    """Build an RST list-table."""
+    data = readfile(infile)
     data = data.splitlines()
     if title is None:
         title = ''
-    col_num = data[0].count('+') -1
-    col_width = int(100 / col_num)
+    col_num = data[0].count('+') - 1
+    col_width = str(int(100 / col_num))
+    col_width = (col_width + ' ') * col_num
 
     output = []
     for line in data:
@@ -78,14 +114,13 @@ def buildTable(infile, outfile, title):
         output.append(row)
     result = ' '.join(output)
 
-    list_table =""".. list-table:: %s
+    list_table = """.. list-table:: %s
    :widths: %s
    :header-rows: 1
-
- %s""" % (title, col_width, result)
+   %s""" % (title, col_width, result)
 
     if outfile:
-        writeRST(outfile, list_table)
+        writefile(outfile, list_table)
     else:
         print(list_table)
     return
@@ -97,9 +132,9 @@ if __name__ == '__main__':
                                      to list-table''')
     parser.add_argument('INPUT', type=str, help='''input RST file containing a
                         single table''')
-    parser.add_argument('-o', type=str, nargs='?', default=None,
-                        help='output RST file')
-    parser.add_argument('-t', type=str, nargs='?', default=None,
-                        help='table title')
+    parser.add_argument('-o', type=str, dest='file', default=None,
+                        help='write the converted table to [FILE]')
+    parser.add_argument('-t', type=str, dest='title', default=None,
+                        help='use [TITLE] as the table title')
     args = parser.parse_args()
-    buildTable(args.INPUT, args.o, args.t)
+    buildTable(args.INPUT, args.file, args.title)
